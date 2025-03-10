80cd4ed4e42ea8f6363f7da7201a49c338a58e07
Stephen Quan <squan@esri.com>
2025-03-10 12:19:55 +1100

    Initial .NET MAUI 9 starter app

---
+++ MauiResXDemo/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace MauiResXDemo;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
